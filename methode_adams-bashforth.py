366b97c2be2c869c89d07b7a78d1cff30cf2e970
Oscar L. <oscar.lousteau.etudes@gmail.com>
2026-02-10 00:17:28 +0100

    quelques petits avancements par rapport à la classe vecteur

--- methode_adams-bashforth.py
+++ methode_adams-bashforth.py
@@ -5,16 +5,16 @@ import typing
 
 
 def ui_plus_un(
-    liste_ui: typing.List[typing.Tuple[int]], f: typing.Callable, ti: float, h: float, m: int
+    liste_ui: typing.List['Vector'], f: typing.Callable[[float,'Vector'],'Vector'], ti: float, h: float, m: int
 ):
     """Calculate the next ui of the list, using the previous ones
 
     Parameters
     ----------
-    liste ui : List[Tuple[int]]
+    liste ui : List['Vector']
         all ui to calculate ui+1, in order (u0,u1,u2 etc...)
     f : function
-        ODE (ordinary differential equation)
+        ODE (ordinary differential equation) that takes in entry a time and a vector and returns a vector
     ti : float
         temps i avec la relation ti+1 = ti + h
     h : int
@@ -27,6 +27,8 @@ def ui_plus_un(
 
 
 
-    ui_plus_1 = liste_ui[-1] + beta * h * sum (tout le reste)
+    ui_plus_1 = list_ui[-1] + beta * h * sum (tout le reste)
 
-    return ui_plus_1
+    res = u_i + h * beta * sum_ODE
+
+    return res
