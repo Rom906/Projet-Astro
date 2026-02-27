1e97fe38f5731607dd548cc6958e0cad61c43da2
Oscar L. <oscar.lousteau.etudes@gmail.com>
2026-02-27 17:59:22 +0100

    com

--- fonction_adams-bashforth.py
+++ fonction_adams-bashforth.py
@@ -2,12 +2,6 @@ import typing
 
 import matplotlib.pyplot as plt
 
-RT = 6371000.0  # Earth radius [m]
-
-
-def normalize_r_position(rp):
-    return rp / RT
-
 
 def ui_plus_un(
     liste_ui: typing.List["Vector"],
@@ -51,28 +45,3 @@ def ui_plus_un(
     ui_plus_1 = liste_ui[-1] + beta_choisi * h * sum
 
     return ui_plus_1
-
-
-# === Trajectory visualization ===
-
-
-def plot_trajectory_2D(rp):
-    plt.plot(rp[:, 0], rp[:, 1])
-    plt.xlabel("X (Earth Radii)")
-    plt.ylabel("Y (Earth Radii)")
-    plt.title("2D Trajectory of Charged Particle")
-    plt.grid()
-    plt.axis("equal")
-    plt.show()
-
-
-def plot_trajectory_3D(rp):
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
-    ax.plot(rp[:, 0], rp[:, 1], rp[:, 2])
-    ax.set_xlabel("X (Earth Radii)")
-    ax.set_ylabel("Y (Earth Radii)")
-    ax.set_zlabel("Z (Earth Radii)")
-    ax.set_title("3D Trajectory of Charged Particle")
-    plt.show()
---
+++ fonctions.py
@@ -0,0 +1,218 @@
+from utils import Vector
+from typing import Callable, List
+import seaborn as sb
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from math import pi, cos, sin
+
+
+def get_intervall(steps: int, minimum: float, maximum: float) -> List[float]:
+    """
+    return a liste of the specified number of values evenly spaced between "minimum" and "maximum" included
+
+    :param steps: number of steps
+    :type steps: int
+    :param minimum: intervall start
+    :type minimum: float
+    :param maximum: intervall stop
+    :type maximum: float
+    :return: a list of steps value evenly spaced between "minimum" and "maximum" included
+    :rtype: List[float]
+    """
+    intervall = []
+    h = (maximum - minimum) / (steps - 1)
+    for i in range(steps):
+        intervall.append(minimum + h * i)
+    return intervall
+
+
+def compute_solution(
+    model: Callable[
+        [List[Vector], Callable[[Vector, float], Vector], float, float, int], Vector
+    ],
+    differential_equation: Callable[[Vector, float], Vector],
+    steps: int,
+    minimum: float,
+    maximum: float,
+    initial_conditions: Vector,
+    multiple_steps_method: bool = False,
+    number_of_steps: int = 1,
+) -> List[Vector]:
+    """
+    compute an approximated solution of the given differential equation using the given model between min and max in a specified number of steps
+
+    :param model: function representing the model used to approximate the solution. It needs to take a specified amount of previous steps to calculate the next one
+    :type model: Callable[[List[Vector], differential_equation_type, float, float, int], Vector]
+    :param differential_equation: represent the differential equation system to approximate. It is a function which represent the f in the equation y' = f(y, t)
+    :type differential_equation: Callable[[Vector, float], Vector]
+    :param steps: number of steps used to approximate the solution
+    :type steps: int
+    :param minimum: the value where we start to compute the approximate solution of the differential equation
+    :type minimum: float
+    :param maximum: the value where we stop to compute the approximate solution of the differential equation
+    :type maximum: float
+    :param initial_conditions: the initial values of the differential equation system
+    :type initial_conditions: Vector
+    :param multiple_steps_method: if true, means that the model used is using multiple steps to compute the solution
+    :type multiple_steps_method: bool
+    :param number_of_steps: if the method is using multiple steps, it is the maximum number of step used by it
+    :type number_of_step: int
+    :return: a list of "steps" approximated value of the differential equation solution
+    :rtype: List[Vector]
+    """
+    solution: List[Vector] = [initial_conditions]
+    h = (maximum - minimum) / (steps - 1)
+    start = minimum + h
+
+    if multiple_steps_method:
+        for i in range(1, number_of_steps):
+            vector_list = []
+            for j in range(i):
+                vector_list.append(solution[-1 - i])
+            solution.append(
+                model(vector_list, differential_equation, minimum + h * i, h, i)
+            )
+        start = minimum - h * number_of_steps
+
+    intervall = get_intervall(steps - 1, start, maximum)
+
+    for ti in intervall:
+        vector_list = []
+        for i in range(number_of_steps):
+            vector_list.append(solution[-1 - i])
+        solution.append(
+            model(vector_list, differential_equation, ti, h, number_of_steps)
+        )
+    return solution
+
+
+def plot_x_solution(time: List[float], solution: List[Vector]) -> None:
+    """
+    plot the x coordinates of the computed solution
+
+    :param time: The list of the different time where the soltution was computed
+    :type time: List[float]
+    :param solution: The computed solution
+    :type solution: List[Vector]
+    """
+    x_coordinate = []
+    for vector in solution:
+        x_coordinate.append(vector[0][0])
+    sb.lineplot(x=time, y=x_coordinate)
+    plt.title("x coordinate of the particule trajectory")
+    plt.grid(True)
+    plt.show()
+
+
+def plot_y_solution(time: List[float], solution: List[Vector]) -> None:
+    """
+    plot the y coordinates of the computed solution
+
+    :param time: The list of the different time where the soltution was computed
+    :type time: List[float]
+    :param solution: The computed solution
+    :type solution: List[Vector]
+    """
+    y_coordinate = []
+    for vector in solution:
+        y_coordinate.append(vector[0][1])
+    sb.lineplot(x=time, y=y_coordinate)
+    plt.title("y coordinate of the particule trajectory")
+    plt.grid(True)
+    plt.show()
+
+
+def plot_z_solution(time: List[float], solution: List[Vector]) -> None:
+    """
+    plot the z coordinates of the computed solution
+
+    :param time: The list of the different time where the soltution was computed
+    :type time: List[float]
+    :param solution: The computed solution
+    :type solution: List[Vector]
+    """
+    z_coordinate = []
+    for vector in solution:
+        z_coordinate.append(vector[0][2])
+    sb.lineplot(x=time, y=z_coordinate)
+    plt.title("z coordinate of the particule trajectory")
+    plt.grid(True)
+    plt.show()
+
+
+def plot_error(
+    approximated_solution: List[Vector], exact_solution: List[Vector], time: List[float]
+) -> None:
+    """
+    plot the error on the position during time of the computed solution compared to an exact (or almost exact) solution
+
+    :param approximated_solution: the computed solution with the choosen model
+    :type approximated_solution: List[vector]
+    :param exact_solution: the exact solution of the equation
+    :type exact_solution: List[Vector]
+    """
+    error = []
+    for i in range(len(exact_solution)):
+        error.append(abs(approximated_solution[i][0] - exact_solution[i][0]) ** 2)
+        print(approximated_solution[i][0])
+    sb.lineplot(x=time, y=error)
+    plt.title("Model error during time")
+    plt.grid(True)
+    plt.show()
+
+
+def plot_3d(positions: List[Vector]) -> None:
+    """
+    make a 3D plot of an ordonated liste of position to represent the trajectory of the studied system
+
+    :param positions: the list of the different position
+    :type positions: List[Vector0]
+    """
+    figure = go.Figure()
+
+    # Plot the position
+    x = []
+    y = []
+    z = []
+    for i in range(len(positions)):
+        x.append(positions[i][0])
+        y.append(positions[i][1])
+        z.append(positions[i][2])
+    figure.add_trace(
+        go.Scatter3d(
+            x=x, y=y, z=z, mode="lines", line=dict(color="blue", width=1, dash="solid")
+        )
+    )
+
+    # Add a sphere at (0, 0, 0) representing earth
+    r = 1
+    phi = get_intervall(30, 0, 2 * pi)
+    theta = get_intervall(15, 0, pi)
+    xe = []
+    ye = []
+    ze = []
+    for i in range(len(phi)):
+        row_x = []
+        row_y = []
+        row_z = []
+        for j in range(len(theta)):
+            row_x.append(r * cos(phi[i]) * sin(theta[j]))
+            row_y.append(r * sin(phi[i]) * sin(theta[j]))
+            row_z.append(r * cos(theta[j]))
+        xe.append(row_x)
+        ye.append(row_y)
+        ze.append(row_z)
+    figure.add_trace(go.Surface(x=xe, y=ye, z=ze, showscale=False))
+
+    # Set parameters
+    figure.update_layout(
+        scene=dict(
+            xaxis=dict(title="x", showgrid=True, zeroline=True),
+            yaxis=dict(title="y", showgrid=True, zeroline=True),
+            zaxis=dict(title="z", showgrid=True, zeroline=True),
+            aspectmode="data",
+        )
+    )
+
+    # Show figure
+    figure.show()
