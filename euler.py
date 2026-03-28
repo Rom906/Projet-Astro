af506139e133d95b4401868708df4d171dddb68b
Oscar L. <oscar.lousteau.etudes@gmail.com>
2026-03-28 18:26:39 +0100

    remise à 0

--- euler.py
+++
@@ -1,8 +0,0 @@
-from utils import Vector
-from typing import List, Callable
-
-
-def euler(liste_ui: List[Vector], f: Callable[[float, Vector], Vector], ti: float, h: float, m: int) -> Vector:
-    vector = liste_ui[0]
-    ui_plus_un = vector + h * f(ti, vector)
-    return ui_plus_un
