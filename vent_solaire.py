e71cf5d016dacfc40aaae25b998757058297e148
Oscar L. <oscar.lousteau.etudes@gmail.com>
2026-03-29 17:42:33 +0200

    distribution normale ajoutée

--- vent_solaire.py
+++ vent_solaire.py
@@ -23,28 +23,33 @@ initial_conditions = convert_to_normalized(
 )
 initial_conditions = Vector([initial_conditions[0], initial_conditions[1]])
 
-N_particules = 100
+N_particules = 10
 
-vitesse_moyenne = Vector([400000.0, 0.0, 0.0])
+vitesse_vent_lent = Vector([400000.0, 400000.0, 400000.0])
+vitesse_vent_rapide = Vector([650000.0, 650000.0, 650000.0])
 
-vitesse_thermique = 40000.0  # Écart-type de 40 km/s
+vitesse_thermique = Vector([40000.0, 40000.0, 40000.0])
 
-vitesse_moyenne_norm = parameters.normalize_velocity(vitesse_moyenne)
+vitesse_moyenne_norm = parameters.normalize_velocity(vitesse_vent_lent)
 
-vitesse_termique_nomr =
+vitesse_termique_nomr = parameters.normalize_velocity(vitesse_thermique)
 
 liste_conditions_initiales = []
 
 
 for i in range(N_particules):
-    vx = 
-    vy = 
-    vz = 
+    vx = np.random.normal(loc=vitesse_moyenne_norm[0], scale=vitesse_termique_nomr)
+    vy = np.random.normal(loc=vitesse_moyenne_norm[0], scale=vitesse_termique_nomr)
+    vz = np.random.normal(loc=vitesse_moyenne_norm[0], scale=vitesse_termique_nomr)
 
     vitesse_particule = Vector([vx, vy, vz])
 
     ci_particule = Vector([initial_conditions[0], vitesse_particule])
     liste_conditions_initiales.append(ci_particule)
 
-print(f"{N_particules} particules générées avec succès !")
+print(f"{N_particules} particules générées avec succès !\n")
 
+# for i in range(5):
+#     print(f"ci pour la {i}eme particule :")
+#     print(f"- position : {liste_conditions_initiales[0][0]}")
+#     print(f"- vitesse : {liste_conditions_initiales[0][1]}\n")
