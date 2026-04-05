cb3ce87452233963d2d00f30a759821f0e015573
tilotogo <teo.pujol1804@gmail.com>
2026-04-05 13:55:46 +0200

    atmosphéric model implementation

--- atmospheric_model.py
+++ atmospheric_model.py
@@ -1,6 +1,35 @@
+from math import e, exp
+
+
 T_inf = 900  # K, can be modified considering different solar activity
 g_a = 9.80665  # m.s-2, gravity
 R_a = 6356.766  # Km, Terestrial rayon
+z_0 = 120000  # m, reference height
+T_0 = 355  # K, température at the reference height z_0
+y = (T_inf - 800) / (750 + 1.722 * (10 ** (-4)) * ((T_inf - 800) ** 2))  # a coefficient
+S = 0.0291 * exp((-(y ** 2) / 2))  # a coefficient
+sigma = S + 1 / (R_a + z_0)  # a coefficient
+a = (T_inf - T_0) / T_inf
+R = 8.31
+Na = 6.022 * (10 ** 23)
+
+
+O2 = 0
+N2 = 1
+HE = 2
+O = 3
+H = 4
+AR = 5
+
+
+def M_i(molecular_index: int) -> float:
+    M = [31.9988, 28.0134, 4.0026, 15.9994, 1.00797, 39.948]
+    return M[molecular_index]
+
+
+def alpha_i(molecular_index: int) -> float:
+    alpha = [0, 0, -0.38, 0, -0.25, 0]
+    return alpha[molecular_index]
 
 
 def g(z: float) -> float:
@@ -12,3 +41,30 @@ def g(z: float) -> float:
     :rtype: float
     """
     return (g_a * R_a ** 2) / ((R_a + z) ** 2)
+
+
+g_0 = g(z_0)
+
+
+def C(z: float) -> float:
+    return (z - z_0) * (R_a + z_0) / (R_a + z)
+
+
+def T(z: float) -> float:
+    return T_inf - (T_inf - T_0) * e ** (-sigma * C(z))
+
+
+def gamma_i(molecular_index: int) -> float:
+    return M_i(molecular_index) * g_0 / (sigma * R * T_inf)
+
+
+def concentration_ni_z0(molecular_index: int) -> float:
+    ni_z0 = [1 * (10 ** 17) / Na, 3 * (10 ** 17) / Na, 1 * (10 ** 13) / Na, 2 * (10 ** 17) / Na, 10 ** 12 / Na, 1 * (10 ** 15) / Na]  # We need to find better values
+    return ni_z0[molecular_index]
+
+
+def concentration_ni(z: float, moleculat_index: int) -> float:
+    return concentration_ni_z0(moleculat_index) * (((1 - a) / (1 - a * e ** (-sigma * C(z)))) ** (1 + alpha_i(moleculat_index) + gamma_i(moleculat_index))) * e ** (-sigma * gamma_i(moleculat_index) * C(z))
+
+
+print(concentration_ni(500000, H) * Na)
