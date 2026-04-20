31a765eabbe7a4f17fa12218e2adf5f8dceb6974
Quadratick <127685525+Quadratick@users.noreply.github.com>
2026-04-20 11:29:17 +0200

    Update aurores.py
    Added Oscar's MB func

--- aurores.py
+++ aurores.py
@@ -17,7 +17,21 @@ def collision_test(conditions: List[Vector], molecule_index) -> bool:
 
 
 def maxwell_boltzmann_test():
-    return Vector([0, 0, 0])
+    from normalization import NormalizationParameters, convert_to_normalized
+from utils import Vector
+from numpy import np
+
+
+def tirage_vitesse_aleatoire(initial_velocity=400000, vitesse_thermique=40000):
+
+    initial_velocity = Vector([initial_velocity, 0, 0])
+
+    vx = np.random.normal(loc=initial_velocity[0], scale=vitesse_thermique)
+    vy = np.random.normal(loc=initial_velocity[1], scale=vitesse_thermique)
+    vz = np.random.normal(loc=initial_velocity[2], scale=vitesse_thermique)
+
+    vitesse_particule_random = Vector([vx, vy, vz])
+    return vitesse_particule_random
 
 
 def cross_section(molecule, electron_nrg):
