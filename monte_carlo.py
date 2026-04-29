362220312041ca15f38142254a6148cf386b9e8d
Quadratick <james05goldie@gmail.com>
2026-04-29 16:05:56 +0200

    Bob the builder

--- monte_carlo.py
+++ monte_carlo.py
@@ -57,21 +57,3 @@ def mass(molecule):
 
 def field_contribution(q_other, r_self, r_other):
     return (q_other) / (4 * pi * e0 * abs(r_other - r_self)**2) * r_other - r_self
-
-def compute_collisional_trajectory(initial_conditions: Vector, differential_equation, h, max_collisions):
-    n_collisions = 0
-    solution = [initial_conditions]
-    collision_positions = []
-    t = 0
-    while n_collisions < max_collisions:
-        new = RK4([solution[-1]], differential_equation, t, h, 1)
-        t += h
-        if collision_test(new[0]):
-            for i in range(3):
-                new[1][i] = -new[1][i]
-            collision_positions.append(new[0])
-            n_collisions += 1
-        print(n_collisions)
-        solution.append(new)
-
-    return solution, collision_positions
