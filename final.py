01c95bd800d0b7aa758200f18610277e3e147f61
Quadratick <james05goldie@gmail.com>
2026-04-29 16:27:43 +0200

    Added a compute_solution for parallelisation

--- final.py
+++ final.py
@@ -10,55 +10,29 @@ from atmospheric_model import O, O2, H, HE, AR, N2
 from random import randrange
 from multiprocessing import Pool
 
-NA = 6
-
-def compute_solution_trash_points_by_steps(initial_conditions: Vector) -> Tuple[Vector, float, int]:
+def compute_solution(initial_conditions: Vector) -> Tuple[Vector, float, int]:
     start_time = time.time()
     params = NormalizationParameters(RT, qe / mp, MO, abs(mu))
     tolerated_variation = 0.05
-    model_n_steps = 1
-    model_order = 4
     max_n_steps = 10000
-    initial_step_size = 0.01
-    h = initial_step_size
-    pos_vel = initial_conditions
+    h = 0.01
     n_steps = 0
     ti = 0
-    collisions = False
-    E = convert_electric_field_to_normalized(Vector([0, 0, 0]), params)
-    # while not collisions:
-    #     E = Vector([0, 0, 0])
-    #     for i in range(other_particles):
-    #         E += field_contribution(-qe, pos_vel[0], other_particles[i][-1][0])
     for i in range(len(initial_conditions)):
-        pos_vel = pos_vels[0]
-        new_step_large = model(
-        [pos_vel], differential_equation, ti, h, model_n_steps, E=E
-        )
-        for i in range(len(molecules_list)):
-            denormalized_posvel = convert_to_dimensional(pos_vel[0], pos_vel[1], params)
-            conditions = Vector([denormalized_posvel[0], denormalized_posvel[1]])
-            
-            if test_collision(conditions, i):
-                collisions = True
-                molecule = i
-        if n_steps > max_n_steps:
-            molecule = NA
-            break
-        if abs(pos_vel[0]) > 50:
-            molecule = NA
-            break
-        initial_conditions[i][1] = pos_vel
+        E = Vector([0, 0, 0])
+        pos_vel = initial_conditions[i][0]
+        for j in range(len(initial_conditions)):
+            if pos_vel[0] != initial_conditions[j][0][0]:
+                E += field_contribution(-qe, pos_vel[0], initial_conditions[j][0][0])
+        E = convert_electric_field_to_normalized(Vector([0, 0, 0]), params)
+        new_pos_vel = RK4([pos_vel], differential_equation_normalized, ti, h, 1, E=E)
+        initial_conditions[i][1] = new_pos_vel
 
 
     comp_time = time.time() - start_time
     print("\n=== Computation Statistics ===")
-    print(f"Method: {model.__name__}")
-    print(f"Number of points: {n_steps}")
     print(f"Computation time: {comp_time:.4f} s")
     print("==============================\n")
-
-    return pos_vel, ti, molecule
 
 
 if __name__ == "__main__":
@@ -74,16 +48,5 @@ if __name__ == "__main__":
         initial_position = RT * Vector([x_start, y_start, z_start])
         initial_velocity = RT * Vector([vx_start, vy_start, vz_start])
         initial_condition = convert_to_normalized(initial_position, initial_velocity, parameters)
-        initial_conditions = Vector([initial_condition[0], initial_condition[1]])
-
-    with Pool() as p:
-        results = p.starmap(compute_solution_trash_points_by_steps, initial_conditions)
-
-    collisions_positions = []
-    times = []
-    molecule_collisions_index = []
-    for result in results:
-        collisions_positions.append(result[0][0])
-        times.append(result[1])
-        molecule_collisions_index.append(result[2])
-    plot_3d_collisions_only(collisions_positions, molecule_collisions_index)
+        initial_condition = [Vector([initial_condition[0], initial_condition[1]]), None]
+        initial_conditions.append(initial_condition)
